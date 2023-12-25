af2f1b5d35781ff3173770c8af681fae6f4c6b26
vinothsrinivasan90 <153488049+vinothsrinivasan90@users.noreply.github.com>
2023-12-25 20:41:11 +0530

    Add files via upload
    Day2  Class and Home assignments are uploaded.

---
+++ day2/edgeBrowser.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class edgeBrowser {
+
+	public static void main(String[] args) {
+		Browser br2 = new Browser();
+		br2.launchBrowser(null);
+		br2.loadUrl();
+
+	}
+
+}
